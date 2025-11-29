6a64b18f5e4987c1d11cd6c26fd8f06f2120616c
ek-ole <ek_34@mail.ru>
2025-11-29 19:21:09 +0300

    feat: add method create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,20 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { User } from './entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
   private users: User[] = [];
+
+  create(createUserDto: CreateUserDto): User {
+    const newUser: User = {
+      id: uuidv4(),
+      login: createUserDto.login,
+      password: createUserDto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
 
   findAll(): User[] {
     return this.users;
   }
 
   findOne(id: string): User {
-    return null;
-  }
-
-  create(createUserDto: CreateUserDto): User {
     return null;
   }
 
