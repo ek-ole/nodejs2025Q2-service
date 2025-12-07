81d5c357ce94ff027c111a7023379f36fc8b9d73
ek-ole <ek_34@mail.ru>
2025-12-07 21:44:35 +0300

    feat: update album entity

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,6 +1,27 @@
+import { Artist } from 'src/artists/entities/artist.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('albums')
 export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar', length: 255 })
   name: string;
+
+  @Column({ type: 'int' })
   year: number;
+
+  @Column({ type: 'uuid', nullable: true })
   artistId: string | null;
+
+  @ManyToOne(() => Artist, { onDelete: 'SET NULL', nullable: true })
+  @JoinColumn({ name: 'artistId' })
+  artist: Artist | null;
 }
