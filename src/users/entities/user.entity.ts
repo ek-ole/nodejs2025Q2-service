7d321c84671ca73020fdf9c8d3c7e587546c050a
ek-ole <ek_34@mail.ru>
2025-12-07 21:36:44 +0300

    feat: update user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,12 +1,41 @@
 import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity('users')
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   login: string;
 
+  @Column({ type: 'varchar', length: 255 })
   @Exclude()
   password: string;
 
+  @Column({ type: 'int', default: 1 })
   version: number;
+
+  @CreateDateColumn({
+    type: 'bigint',
+    transformer: {
+      to: (value: Date) => value.getTime(),
+      from: (value: number) => new Date(value),
+    },
+  })
   createdAt: number;
+
+  @UpdateDateColumn({
+    type: 'bigint',
+    transformer: {
+      to: (value: Date) => value.getTime(),
+      from: (value: number) => new Date(value),
+    },
+  })
   updatedAt: number;
 }
