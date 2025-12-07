a2b0c384a86ecfcc6cbf47971b9b7b8c2a4c10aa
ek-ole <ek_34@mail.ru>
2025-12-07 20:40:37 +0300

    feat: add typeorm config

---
+++ src/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeormConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'library',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.TYPEORM_LOGGING === 'true',
+};
