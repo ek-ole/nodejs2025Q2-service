dde3fbea66fbd4826f41b1aa8371a3108884fdb4
ek-ole <ek_34@mail.ru>
2025-12-08 03:17:51 +0300

    fix: fix migration

--- src/migrations/1741462400000-InitialSchema.ts
+++ src/migrations/1741462400000-InitialSchema.ts
@@ -1,10 +1,9 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InitialSchema1741462400000 implements MigrationInterface {
-  name = 'InitialSchema1741462400000'
+  name = 'InitialSchema1741462400000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Создадим таблицы на основе твоих entity
     await queryRunner.query(`
       CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
       
@@ -13,8 +12,8 @@ export class InitialSchema1741462400000 implements MigrationInterface {
         "login" character varying(255) NOT NULL,
         "password" character varying(255) NOT NULL,
         "version" integer NOT NULL DEFAULT '1',
-        "createdAt" bigint NOT NULL,
-        "updatedAt" bigint NOT NULL,
+        "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+        "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
         CONSTRAINT "UQ_2d443082eccd5198f95f2a36e2c" UNIQUE ("login"),
         CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
       );
