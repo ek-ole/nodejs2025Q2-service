93f450bd9641f9e186831bd38d5917966deb3093
ek-ole <ek_34@mail.ru>
2025-12-15 00:04:43 +0300

    feat: create refresh token

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -5,6 +5,7 @@ import { Artist } from './artists/entities/artist.entity';
 import { Favorite } from './favorites/entities/favorite.entity';
 import { Track } from './tracks/entities/track.entity';
 import { User } from './users/entities/user.entity';
+import { RefreshToken } from './auth/entities/refresh-token.entity';
 
 dotenv.config();
 
@@ -17,5 +18,5 @@ export const typeormConfig: DataSourceOptions = {
   database: process.env.DB_DATABASE || 'library',
   synchronize: process.env.DB_SYNCHRONIZE === 'false',
   logging: process.env.TYPEORM_LOGGING === 'true',
-  entities: [User, Artist, Album, Track, Favorite],
+  entities: [User, Artist, Album, Track, Favorite, RefreshToken],
 };
