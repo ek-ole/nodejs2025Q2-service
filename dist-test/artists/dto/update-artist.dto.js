32e91370889719394359f7560db0704a5f9221fd
ek-ole <ek_34@mail.ru>
2025-12-15 12:42:42 +0300

    fix: fix ignore files

--- dist-test/artists/dto/update-artist.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateArtistDto = void 0;
-const swagger_1 = require("@nestjs/swagger");
-const create_artist_dto_1 = require("./create-artist.dto");
-class UpdateArtistDto extends (0, swagger_1.PartialType)(create_artist_dto_1.CreateArtistDto) {
-}
-exports.UpdateArtistDto = UpdateArtistDto;
